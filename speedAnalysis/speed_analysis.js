43e3ce0ab5dea0e04d472a2b3122cb3765533f14
hgcassidy <hgcassidy@gmail.com>
2024-10-29 15:13:16 -0400

    add lenght

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -37,8 +37,10 @@ function endTest() {
     }
 
     // Display the results
+    var typeLength = userTypedText.length
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Lenght: " + typeLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
