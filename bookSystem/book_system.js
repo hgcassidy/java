467549884f76a9059ca3ae16c7e0706cb3a8f707
hgcassidy <hcassidy@gmail.com>
2024-10-29 14:40:51 -0400

    message

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -21,18 +21,24 @@ function addBook() {
 }
 
 function showbooks() {
-    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
+    const booksDiv = books.map((book, index) => `
+        <h1>Book Number: ${index + 1}</h1>
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
-    );
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete</button>
+    `); 
     document.getElementById('books').innerHTML = booksDiv.join('');
-
+}
     function clearInputs() {
         document.getElementById('bookName').value = '';
         document.getElementById('authorName').value = '';
         document.getElementById('bookDescription').value = '';
         document.getElementById('pagesNumber').value = '';
 }
+function deleteBook(index) {
+    books.splice(index, 1); // Elimina el libro en la posición "index"
+    showbooks(); // Actualiza la lista de libros en pantalla
+    clearInputs();
 }
